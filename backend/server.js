4df397a96da0665cd0f4a5739985afbe70f4a552
omi <omi@omi.lan>
2025-01-12 03:14:14 +0530

    dotenv

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 5000;
 
 app.post("/generate", async (req, res) => {
   const { name, vpa, amount } = req.body;
@@ -23,7 +24,7 @@ app.post("/generate", async (req, res) => {
   }
 });
 
-const PORT = 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
